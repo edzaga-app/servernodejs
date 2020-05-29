91cbabbb0c1dbdd3bc356c044a440be574202ad2
edwar-zapata <edwar.zapata@utp.edu.co>
2020-05-29 16:59:06 -0500

    Actualización del nombre del servidor

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello servernodejs'));
+app.get('/', (req, res) => res.send('servernodejs it works!'));
 app.listen(3000);
 
 console.log('Server runing on port ', 3000);
